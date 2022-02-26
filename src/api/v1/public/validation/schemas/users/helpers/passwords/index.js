dd16d7e4c77b6a71856e7aa7b47193fac5865141
beanc16 <bean.christopher@outlook.com>
2022-02-26 12:32:46 -0500

    Update password export

--- src/api/v1/public/validation/schemas/users/helpers/passwords/index.js
+++ src/api/v1/public/validation/schemas/users/helpers/passwords/index.js
@@ -1,15 +1,5 @@
-const passwordStrengthsNone = require("./passwordStrengthNone");
-const passwordStrengthsLow = require("./passwordStrengthLow");
-const passwordStrengthsFair = require("./passwordStrengthFair");
-const passwordStrengthsGood = require("./passwordStrengthGood");
-const passwordStrengthsExcellent = require("./passwordStrengthExcellent");
+const passwordStrengthMap = require("./passwordStrengthMap");
 
 
 
-module.exports = {
-    ...passwordStrengthsNone,
-    ...passwordStrengthsLow,
-    ...passwordStrengthsFair,
-    ...passwordStrengthsGood,
-    ...passwordStrengthsExcellent,
-}
+module.exports = passwordStrengthMap
