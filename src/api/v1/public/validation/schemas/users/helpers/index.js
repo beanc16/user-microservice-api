05fdb7219803c5f46020e71998e61b06128ba367
beanc16 <bean.christopher@outlook.com>
2022-02-26 12:33:20 -0500

    Export passwordStrengthMap from helpers

--- src/api/v1/public/validation/schemas/users/helpers/index.js
+++ src/api/v1/public/validation/schemas/users/helpers/index.js
@@ -1,9 +1,11 @@
 const emailStrings = require("./emailString");
 const usernameStrings = require("./usernameString");
+const passwordStrengthMap = require("./passwords");
 
 
 
 module.exports = {
     ...emailStrings,
     ...usernameStrings,
+    passwordStrengthMap,
 };
